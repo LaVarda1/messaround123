26821fd409f4875b4e81045f52da9117d35d85b5
Joe Lukacovic <efessel@gmail.com>
2023-01-13 10:55:34 -0500

    updates to faq

--- src/app/web/components/page/Faq.vue
+++ src/app/web/components/page/Faq.vue
@@ -17,19 +17,20 @@ type QA = {
 
 const qas = [{
   question: 'What is a pak file?',
-  answer: `A pak file is an archive of files that contain game assets like maps, models, sounds, etc. 
-   The base game shipped with two pak files - pak0.pak and pak1.pak. The first one, pak0.pak, shipped with the shareware 
-   version of quake which was freely distributable.  The pak1.pak was only shipped with the purchased version which is why
+  answer: `<p>A pak file is an archive of files that contain game assets like maps, models, sounds, etc. 
+   The base game shipped with two pak files - pak0.pak and pak1.pak. 
+   <p>The first one, pak0.pak, shipped with the shareware 
+   version of quake which was freely distributable.  
+   <p>The pak1.pak file was only shipped with the purchased version which is why
    this site cannot provide this file for you.  This file is also required to play any modified games or maps.`
 }, {
   question: 'Where can I buy quake?',
-  answer: `GoG or Steam still sell quake for ~$10.  It usually goes on sale for under $5, as of this writing, 
-    it's currently $4. `
+  answer: `<p>You can find purchasable copies of quake on <a href="https://www.gog.com/en/game/quake_the_offering" target="_blank">GoG</a> or <a href="https://store.steampowered.com/agecheck/app/2310/" target="_blank">Steam</a> which sell for ~$10.  It regularly goes on sale for under $5. After the purchase, locate and upload the pak1.pak, which was provided during the install, in the <a href="/setup/assets">setup</a> section.`
 }, {
   question: 'Where is my pak1.pak file?',
-  answer: `The pak1 file is in the id1 directory of your quake installation.  Your quake installation can 
-  depend where you bought it.  For steam on windows, this is C:\Program Files (x86)\Steam\steamapps\common\Quake.  
-  GoG may install here C:\GOG Games\Quake.`
+  answer: `<p>The pak1 file is in the idt1 direcory of your quake installation.  Your quake installation can 
+  depend where you bought it, what OS you run, and if you have customized the install path.  <br>For steam on windows, this is C:\\Program Files (x86)\\Steam\\steamapps\\common\\Quake.  
+  <br>GoG may install here C:\\GOG Games\\Quake.`
 }, {
   question: 'How do I assign a key to the rocket launcher?',
   answer: `The original quake had limited options in the options menu. Sadly, assigning weapons to keys is also missing. 
@@ -46,7 +47,7 @@ const qas = [{
           <li>7 - Rocket Launcher</li>
           <li>8 - Lightning gun</li>
         </ul>
-      <p> You can also edit your <pre>config.cfg</pre> in setup and add these binds directly. They apply when you start
+      <p> You can also edit your <pre>config.cfg</pre> in <a href="/setup/config">setup</a> and add these binds directly. They apply when you start
         the game.
       <p>This can also be used to assign other actions.`
 }]
